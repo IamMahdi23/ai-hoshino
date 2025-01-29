8da4e58f8fb5bd6450e2f717ee228ec31d358743
M A H D I <137701044+IamMahdi23@users.noreply.github.com>
2025-01-29 14:30:13 +0600

    Update commands.py

--- cogs/commands.py
+++ cogs/commands.py
@@ -1,8 +1,1 @@
-# commands.py (inside the cogs folder)
 
-from discord.ext import commands
-
-@commands.command()
-async def ping(ctx):
-    print("Ping command triggered!")
-    await ctx.send("Pong!")
