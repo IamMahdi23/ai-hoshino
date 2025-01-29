4be40c05c02a7b15ea55b11fc733256c41df8770
M A H D I <137701044+IamMahdi23@users.noreply.github.com>
2025-01-29 14:01:04 +0600

    Create main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+import discord
+from discord.ext import commands
+import os
+
+intents = discord.Intents.default()
+intents.message_content = True  # Add the privileged intent
+
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'We have logged in as {bot.user}')
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send('Pong! 🏓')
+
+bot.run(os.getenv("TOKEN"))  # Token stored in GitHub secrets
